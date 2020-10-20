700ed5694fd734e2c08673ae949d008db2e14f84
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-10-20 22:31:45 +0100

    add todo component and used props

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-export default function Todo() {
+export default function Todo(props) {
     return (
         <li className="todo stack-small">
             <div className="c-cb">
-                <input id="todo-1" type="checkbox" />
+                <input id="todo-1" type="checkbox" defaultChecked={props.completed}/>
                 <label className="todo-label" htmlFor="todo-1">
-                    Sleep
+                    {props.name}
             </label>
             </div>
             <div className="btn-group">
                 <button type="button" className="btn">
-                    Edit <span className="visually-hidden">Sleep</span>
+                    Edit <span className="visually-hidden">{props.name}</span>
                 </button>
                 <button type="button" className="btn btn__danger">
-                    Delete <span className="visually-hidden">Sleep</span>
+                    Delete <span className="visually-hidden">{props.name}</span>
                 </button>
             </div>
         </li>
