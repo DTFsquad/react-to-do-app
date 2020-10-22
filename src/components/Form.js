7bd1ef317ffc02cc159e16ac4d43c682b3876032
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-10-23 00:01:00 +0100

    use of state and useState hook

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,17 @@
-import React from "react";
-
-
+import React, { useState } from "react";
 
 
 function Form(props) {
+
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+    const [name, setName] = useState('');
+
     function handleSubmit(e) {
         e.preventDefault();
-        props.addTask("Say hello!");
+        props.addTask(name);
+        setName("");
     }
 
     return (
@@ -14,7 +19,7 @@ function Form(props) {
             <h2 className="label-wrapper">
                 <label htmlFor="new-todo-input" className="label__lg">
                     What needs to be done?
-        </label>
+                </label>
             </h2>
             <input
                 type="text"
@@ -22,6 +27,8 @@ function Form(props) {
                 className="input input__lg"
                 name="text"
                 autoComplete="off"
+                value={name}
+                onChange={handleChange}
             />
             <button type="submit" className="btn btn__primary btn__lg">
                 Add
