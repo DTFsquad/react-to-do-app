f0152ea505e3c14ab463bc53184fc7ddf469d756
Florin Dumitru <dumitruflorin19@yahoo.com>
2020-10-23 00:08:15 +0100

    counting task with ternary operator

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ function App(props) {
     />
   )
   );
+
+  const tasksNoun = taskList.lenght !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <Form addTask={addTask} />
@@ -30,7 +34,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
